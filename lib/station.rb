f000b1c661fb4f2964c3faeee2636725744a173d
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-21 20:56:38 +0100

    Station allows passengers to exit the station.

--- lib/station.rb
+++ lib/station.rb
@@ -11,7 +11,12 @@ class Station
 	end
 
 	def allow_entry(passenger)
+		raise "You have insufficient funds on your Mussel Card." if passenger.mussel_balance<2
 		@passengers << passenger if passenger.mussel_balance>=2
+	end
+
+	def allow_exit(passenger)
+		@passengers.delete(passenger)
 	end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -15,5 +15,20 @@ describe Station do
 
 		expect(station.passengers_in_station.count).to eq(1)	
 	end
-	
+
+	it 'should raise an error if passenger has insufficient credit' do
+		passenger = double :passenger
+		allow(passenger).to receive(:mussel_balance).and_return(1)
+
+		expect{station.allow_entry(passenger)}.to raise_error(RuntimeError)
+	end
+
+	it 'should allow a person to leave when they disembark a train' do
+		passenger = double :passenger
+		allow(passenger).to receive(:mussel_balance).and_return(2)
+		station.allow_entry(passenger)
+		station.allow_exit(passenger)
+
+		expect(station.passengers_in_station.count).to eq(0)
+	end	
 end
